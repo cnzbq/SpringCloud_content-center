5dafefe24f2535e20badbebe24565b1c6f9185d8
zbq <zbqdwq@gmail.com>
2019-09-10 23:44:32 +0800

    整合Nacos服务发现

--- src/main/java/cn/zbq/springcloud/contentcenter/TestController.java
+++ src/main/java/cn/zbq/springcloud/contentcenter/TestController.java
@@ -3,6 +3,8 @@ package cn.zbq.springcloud.contentcenter;
 import cn.zbq.springcloud.contentcenter.dao.content.ShareMapper;
 import cn.zbq.springcloud.contentcenter.domain.entity.content.Share;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +21,11 @@ import java.util.List;
 public class TestController {
     @Autowired
     private ShareMapper shareMapper;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/test")
-    public List<Share> testInsert(){
+    public List<Share> testInsert() {
         // 1. 做插入
         Share share = new Share();
         share.setCreateTime(new Date());
@@ -35,5 +39,18 @@ public class TestController {
         // 2. 查询所欲 查询当前数据库所有share 执行 select * from share
         List<Share> shares = this.shareMapper.selectAll();
         return shares;
+    }
+
+    /**
+     * 测试： 服务发现，证明内容中心总能找到用户中心
+     *
+     * @return 用户中心所有实例的地址信息
+     */
+    @GetMapping("/test2")
+    public List<ServiceInstance> getInstances() {
+        // 查询指定服务的所有实例的信息
+        // DiscoveryClient 由spring提供
+        // consul/eureka/zookeeper...
+        return this.discoveryClient.getInstances("user-center");
     }
 }
