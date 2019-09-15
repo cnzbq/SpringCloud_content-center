a7a4218b0f9b15128fd26601247c902e510f2dc0
zbq <zbqdwq@gmail.com>
2019-09-15 20:32:11 +0800

    5-6为内容中心引入服务发现

--- src/main/java/cn/zbq/springcloud/contentcenter/service/content/ShareService.java
+++ src/main/java/cn/zbq/springcloud/contentcenter/service/content/ShareService.java
@@ -5,10 +5,15 @@ import cn.zbq.springcloud.contentcenter.domain.dto.content.ShareDTO;
 import cn.zbq.springcloud.contentcenter.domain.dto.user.UserDTO;
 import cn.zbq.springcloud.contentcenter.domain.entity.content.Share;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * 分享服务
@@ -16,11 +21,13 @@ import org.springframework.web.client.RestTemplate;
  * @author Zbq
  * @since 2019/9/8 17:57
  */
+@Slf4j
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ShareService {
     private final ShareMapper shareMapper;
     private final RestTemplate restTemplate;
+    private final DiscoveryClient discoveryClient;
 
     /**
      * 通过id查询分享详情
@@ -33,9 +40,17 @@ public class ShareService {
         Share share = this.shareMapper.selectByPrimaryKey(id);
         // 发布人id
         Integer userId = share.getUserId();
+        // 拿到用户中心所有实例
+        List<ServiceInstance> userCenterInstanceList = discoveryClient.getInstances("user-center");
+        String url = userCenterInstanceList.stream()
+                // 数据变换
+                .map(serviceInstance -> serviceInstance.getUri() + "/users/{id}")
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("当前没有实例"));
+        log.info("请求的目标地址{}", url);
         // 调用用户微服务的 /users/{userId}
         UserDTO userDTO = this.restTemplate.getForObject(
-                "http://localhost:8080/users/{id}",
+                url,
                 UserDTO.class, userId
         );
         // 消息的装配
