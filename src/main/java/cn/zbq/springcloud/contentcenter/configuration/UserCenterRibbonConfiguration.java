f83cdf73f8c5f906e3c9dee4a174f5d560fdd43e
zbq <zbqdwq@gmail.com>
2019-09-18 22:47:33 +0800

    通过java代码配置规则实现ribbon随机访问用户中心

---
+++ src/main/java/cn/zbq/springcloud/contentcenter/configuration/UserCenterRibbonConfiguration.java
@@ -0,0 +1,17 @@
+package cn.zbq.springcloud.contentcenter.configuration;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Configuration;
+import ribbonconfiguration.RibbonConfiguration;
+
+/**
+ * ribbon config
+ *
+ * @author Zbq
+ * @since 2019/9/18 22:37
+ */
+@Configuration
+@RibbonClient(name = "user-center", configuration = RibbonConfiguration.class)
+public class UserCenterRibbonConfiguration {
+
+}
---
+++ src/main/java/ribbonconfiguration/RibbonConfiguration.java
@@ -0,0 +1,20 @@
+package ribbonconfiguration;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * TODO
+ *
+ * @author Zbq
+ * @since 2019/9/18 22:39
+ */
+@Configuration
+public class RibbonConfiguration {
+    @Bean
+    public IRule ribbonRule() {
+        return new RandomRule();
+    }
+}
