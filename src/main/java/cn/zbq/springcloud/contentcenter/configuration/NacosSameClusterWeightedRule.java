1d8d7ebdae042dfcf5a7a2c7af8fcad8c6c49d97
zbq <zbqdwq@gmail.com>
2019-11-03 13:25:16 +0800

    基于元数据的版本控制&优先同集群调用

--- src/main/java/cn/zbq/springcloud/contentcenter/configuration/NacosSameClusterWeightedRule.java
+++ src/main/java/cn/zbq/springcloud/contentcenter/configuration/NacosSameClusterWeightedRule.java
@@ -6,6 +6,7 @@ import com.alibaba.nacos.api.exception.NacosException;
 import com.alibaba.nacos.api.naming.NamingService;
 import com.alibaba.nacos.api.naming.pojo.Instance;
 import com.alibaba.nacos.client.naming.core.Balancer;
+import com.alibaba.nacos.client.utils.StringUtils;
 import com.netflix.client.config.IClientConfig;
 import com.netflix.loadbalancer.AbstractLoadBalancerRule;
 import com.netflix.loadbalancer.BaseLoadBalancer;
@@ -40,17 +41,31 @@ public class NacosSameClusterWeightedRule extends AbstractLoadBalancerRule {
             BaseLoadBalancer loadBalancer = (BaseLoadBalancer) this.getLoadBalancer();
             // 想要请求的微服务名称
             String name = loadBalancer.getName();
+            String targetVersion = nacosDiscoveryProperties.getMetadata().get("target-version");
             // 服务发现的API
             NamingService namingService = nacosDiscoveryProperties.namingServiceInstance();
-            // 1.找到指定服务的所有实例 A
+            // 1.找到指定服务的所有实例
             List<Instance> instanceList = namingService.selectInstances(name, true);
-            // 2.过滤出同集群下的所有实例B
-            List<Instance> clusterInstanceList = instanceList.stream()
-                    .filter(instance -> clusterName.equals(instance.getClusterName())).collect(Collectors.toList());
-            // 3.如果B为空，则使用A
-            if (CollectionUtils.isEmpty(clusterInstanceList)) {
-                log.warn("发生跨服务调用");
-                clusterInstanceList = instanceList;
+            // 2.如果配置了版本映射，则过滤版本
+            if (StringUtils.isNotEmpty(targetVersion)) {
+                instanceList = instanceList.stream()
+                        .filter(instance -> targetVersion.equals(instance.getMetadata().get("version"))).collect(Collectors.toList());
+                if (CollectionUtils.isEmpty(instanceList)) {
+                    log.warn("未找到对应版本的实例，请检查配置，targetVersion:{},instance:{}", targetVersion, instanceList);
+                    return null;
+                }
+            }
+            // 3.如果配置了集群，优先同集群实例
+            List<Instance> clusterInstanceList = instanceList;
+            if (StringUtils.isNotEmpty(clusterName)) {
+                clusterInstanceList = instanceList.stream()
+                        .filter(instance -> clusterName.equals(instance.getClusterName()))
+                        .collect(Collectors.toList());
+                // 如果同集群实例为空，则使用全部实例
+                if (CollectionUtils.isEmpty(clusterInstanceList)) {
+                    clusterInstanceList = instanceList;
+                    log.warn("发生跨集群调用。clusterName = {}, targetVersion = {}, clusterInstanceList = {}", clusterName, targetVersion, clusterInstanceList);
+                }
             }
             // 4.基于权重的负载均衡算法，返回一个实例
             Instance instance = ExtendBalancer.getHostByRandomWeight2(clusterInstanceList);
