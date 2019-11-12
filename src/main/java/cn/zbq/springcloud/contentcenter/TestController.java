66cbf350a88cbb243c69e6afb129a6551e953dec
zbq <zbqdwq@gmail.com>
2019-11-12 21:17:22 +0800

    8-10 代码配置规则

--- src/main/java/cn/zbq/springcloud/contentcenter/TestController.java
+++ src/main/java/cn/zbq/springcloud/contentcenter/TestController.java
@@ -6,6 +6,9 @@ import cn.zbq.springcloud.contentcenter.domain.entity.content.Share;
 import cn.zbq.springcloud.contentcenter.feignclient.TestBaiduFeignClient;
 import cn.zbq.springcloud.contentcenter.feignclient.TestUserCenterFeignClient;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -95,5 +99,22 @@ public class TestController {
     public String testHot(@RequestParam(required = false) String a, @RequestParam(required = false) String b) {
 
         return a + " " + b;
+    }
+
+    @GetMapping("test-add-flow-rule")
+    public String testHot() {
+        this.initFlowQpsRule();
+        return "aaaaaaaa";
+    }
+
+    private void initFlowQpsRule() {
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule("shares/1");
+        // set limit qps to 20
+        rule.setCount(20);
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        rule.setLimitApp("default");
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
     }
 }
